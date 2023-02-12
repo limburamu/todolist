26c219b8c43021b5730ece6577a61532965e94bb
limburamu <limburamu23@gmail.com>
2023-02-12 21:46:44 +0545

    todolist

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
 
+import AddTodo from './components/AddTodo';
+import TodoItem from './components/TodoItem';
+import { useState } from 'react';
+
+import { BrowserRouter } from 'react-router-dom';
+
 function App() {
+
+  const [todo, setTodo] = useState([])
+
+
+  const getItem = (item) => {
+    setTodo((prevState) => {
+      return [...prevState, item]
+    })
+  }
+
+  const getCompleted = (id) => {
+    setTodo((prevState) => {
+      return prevState.filter((item, index) => {
+        return index !== id;
+      })
+    })
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      {/* <div className="todo"> */}
+      <AddTodo item={getItem} />
+      {todo && todo.map((todo, index) => (
+        <TodoItem id={index} item={todo} completed={getCompleted} />
+      ))}
+      {/* </div> */}
+    </BrowserRouter>
   );
 }
 
